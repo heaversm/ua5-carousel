b8d36e2cd8d45e4ebf1e76c914c5fc85fd902754
Mike Heavers <mheavers@gmail.com>
2020-07-30 08:30:15 -0700

    initial responsive fluid type reversible star graphic

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -3,8 +3,10 @@ import cstyles from './Carousel.module.scss';
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Slider from 'react-slick';
+import cx from 'classnames';
 
 import { messageData } from '../../data/messageData';
+import star from '../../assets/star.svg';
 
 const Carousel = props => {
   const [curSlide, setSlide] = useState(0);
@@ -19,10 +21,12 @@ const Carousel = props => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    dotsClass: cx(cstyles.dots, 'slick-dots'),
   };
 
   return (
     <div className={cstyles.container}>
+      <img className={cx(cstyles.star, cstyles["star--left"])} src={star} alt="asterisk decorative graphic"/>
       <h1 className={cstyles.section_title}>{messageData[0].section_title}</h1>
       <Slider {...carouselSettings} className={cstyles.slider}>
         {messageData[0].quotes.map((quote, index) => {
