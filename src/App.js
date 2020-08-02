6e38d4e5647d52b8519d9fc07e39535be9c3e767
Mike Heavers <mheavers@gmail.com>
2020-08-02 07:35:45 -0700

    proptypes props for various slider configuration options

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './styles/app.scss';
 import { Carousel } from './components/';
+import { messageData } from './data/messageData';
 
 function App() {
   return (
     <div className="UA5">
       <main>
-        <Carousel />
+        <Carousel 
+          messagesData={messageData[0]}
+        />
+        <Carousel 
+          starLeft
+          messagesData={messageData[1]}
+          messageColor='#0E4EF3'
+          buttonColor='#0E4EF3'
+          doFade={true}
+          doAutoplay={true}
+          animSpeed={1000}
+          autoplaySpeed={10000}
+        />
       </main>
     </div>
   );
--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,48 +1,77 @@
 import cstyles from './Carousel.module.scss';
 
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Slider from 'react-slick';
 import cx from 'classnames';
-
-import { messageData } from '../../data/messageData';
 import star from '../../assets/star.svg';
 
-const Carousel = props => {
-  const [curSlide, setSlide] = useState(0);
-
-  const changeSlide = slideVal => {
-    setSlide(slideVal);
-  };
+const Carousel = ({starLeft,messagesData, messageColor, buttonColor, showArrows, showDots, doFade, doAutoplay, animSpeed, autoplaySpeed}) => {
 
   const carouselSettings = {
-    dots: true,
     infinite: true,
-    speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     dotsClass: cx(cstyles.dots, 'slick-dots'),
+    arrows: showArrows,
+    dots: showDots,
+    fade: doFade,
+    autoplay: doAutoplay,
+    autoplaySpeed: autoplaySpeed,
+    speed: animSpeed
   };
 
   return (
     <div className={cstyles.container}>
-      <img className={cx(cstyles.star, cstyles["star--left"])} src={star} alt="asterisk decorative graphic"/>
-      <h1 className={cstyles.section_title}>{messageData[0].section_title}</h1>
-      <Slider {...carouselSettings} className={cstyles.slider}>
-        {messageData[0].quotes.map((quote, index) => {
-          return (
-            <div className={cstyles.slide} key={`quote-${index}`}>
-              <h2 className={cstyles.message}>{quote.message}</h2>
-              <h3 className={cstyles.attribution}>{quote.attribution}</h3>
-            </div>
-          );
-        })}
-      </Slider>
-      <button className={cstyles.view_all}>VIEW ALL</button>
+      <img 
+        className={`${cx(cstyles.star)} ${starLeft && cx(cstyles['star--left'])}`}
+        src={star} alt="asterisk decorative graphic"
+      />
+      <div className={cstyles.container_inner}>
+        <h1 className={cstyles.section_title}>{messagesData.section_title}</h1>
+        <Slider {...carouselSettings} className={cstyles.slider}>
+          {messagesData.quotes.map((quote, index) => {
+            return (
+              <div className={cstyles.slide} key={`quote-${index}`}>
+                <h2 className={cstyles.message} style={{color: messageColor && messageColor}}>{quote.message}</h2>
+                <h3 className={cstyles.attribution}>{quote.attribution}</h3>
+              </div>
+            );
+          })}
+        </Slider>
+        <button className={cstyles.view_all} style={{backgroundColor: buttonColor && buttonColor}}>VIEW ALL</button>
+      </div>
     </div>
   );
 };
 
-Carousel.propTypes = {};
+Carousel.defaultProps = {
+  starLeft: false,
+  showArrows: true,
+  showDots: true,
+  doFade: false,
+  doAutoplay: false,
+  animSpeed: 500,
+  autoplaySpeed: 3000,
+};
+
+Carousel.propTypes = {
+  starLeft: PropTypes.bool,
+  showArrows: PropTypes.bool,
+  showDots: PropTypes.bool,
+  doFade: PropTypes.bool,
+  doAutoplay: PropTypes.bool,
+  animSpeed: PropTypes.number,
+  autoplaySpeed: PropTypes.number,
+  messageColor: PropTypes.string,
+  buttonColor: PropTypes.string,
+  messagesData: PropTypes.shape({
+    section_title: PropTypes.string,
+    quotes: PropTypes.arrayOf(PropTypes.shape({
+      message: PropTypes.string.isRequired,
+      attribution: PropTypes.string
+    })).isRequired
+  }),
+};
 
 export default Carousel;
